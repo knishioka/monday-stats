d90b897e1dfa1acbe2a910c047ce60e8db3e8d75
Kenichiro Nishioka <nishioka@3idea.jp>
2020-02-09 12:52:29 +0900

    Add item class

---
+++ item.py
@@ -0,0 +1,11 @@
+class Item():
+    """Monday item class."""
+
+    def __init__(self, name):
+        """Initialize Item class.
+
+        Args:
+            name (str): item name.
+
+        """
+        self.name = name
