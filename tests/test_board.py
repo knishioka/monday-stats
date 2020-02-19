b0016c253a2574d80acfae022c6d1f49b2c028c3
Kenichiro Nishioka <nishioka@3idea.jp>
2020-02-19 20:26:04 +0900

    Test board to pandas.DataFrame

--- tests/test_board.py
+++ tests/test_board.py
@@ -18,6 +18,28 @@ def board_item_dict():
                 {'title': 'col_1', 'text': 'val 1-1'},
                 {'title': 'col_2', 'text': 'val 2-1'}
             ]
+        },
+        {
+            'name': 'item name2',
+            'group': {
+                'id': 'group_id_1',
+                'title': 'group_title_1'
+            },
+            'column_values': [
+                {'title': 'col_1', 'text': 'val 1-2'},
+                {'title': 'col_2', 'text': 'val 2-2'}
+            ]
+        },
+        {
+            'name': 'item name3',
+            'group': {
+                'id': 'group_id_2',
+                'title': 'group_title_2'
+            },
+            'column_values': [
+                {'title': 'col_1', 'text': 'val 1-3'},
+                {'title': 'col_2', 'text': 'val 2-3'}
+            ]
         }
     ]
     return board_items
@@ -34,3 +56,9 @@ def test_board():
 def test_board_with_items(board_item_dict):
     board = Board(board_id=1, board_name='board name', items=board_item_dict)
     assert 'group_title_1' in board.groups.keys()
+
+
+def test_to_dataframe(board_item_dict):
+    board = Board(board_id=1, board_name='board name', items=board_item_dict)
+    df = board.to_dataframe()
+    assert 'col_1' in df.columns and 'col_2' in df.columns
